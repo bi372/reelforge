092da8bcd8ab38939d0afb4dc8d2c4c8db1aee13
bi372 <bi@kiwiecollection.com>
2026-06-04 12:17:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -86,11 +86,13 @@ function wrapText(text, maxChars) {
 }
 
 let mainWin = null
-autoUpdater.autoDownload = true
+autoUpdater.autoDownload = false
 autoUpdater.autoInstallOnAppQuit = true
-autoUpdater.on('update-available', () => { if(mainWin) mainWin.webContents.send('update-available', 'new version') })
+autoUpdater.on('update-available', (info) => { if(mainWin) mainWin.webContents.send('update-available', info.version) })
 autoUpdater.on('download-progress', (p) => { if(mainWin) mainWin.webContents.send('update-progress', Math.round(p.percent)) })
-autoUpdater.on('update-downloaded', () => { if(mainWin) mainWin.webContents.send('update-available', 'ready'); setTimeout(()=>autoUpdater.quitAndInstall(),3000) })
+autoUpdater.on('update-downloaded', () => { if(mainWin) mainWin.webContents.send('update-downloaded') })
+ipcMain.on('start-update-download', () => { autoUpdater.downloadUpdate() })
+ipcMain.on('install-update', () => { autoUpdater.quitAndInstall() })
 
 function createWindow() {
   const win = new BrowserWindow({
